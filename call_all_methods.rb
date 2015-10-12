fba2025542bc33de8be51c8f47088106f23fd453
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2015-10-12 16:26:20 +0200

    return error and success calls in a dictionary

--- call_all_methods.rb
+++ call_all_methods.rb
@@ -1,16 +1,27 @@
-def call_all_methods(object, *args) 
+def call_all_methods(object, *args)
   # remove methods that modify the PRY environment or are too verbose
+  success = []
+  error = []
   exclusions = [:pry,
                 :methods,
                 :private_methods,
                 :public_methods].map { |x| x.to_s}
-  object.methods.map { |x|
-    begin
-      unless exclusions.include? x.to_s then
-        [method(x), object.send(x, *args)]
+  object.methods.each { |x|
+    unless exclusions.include? x.to_s then
+      begin
+        if (args.empty?) then
+          success << [x, object.send(x)]
+        else
+          success << [x, object.send(x, *args)]
+        end
+      rescue StandardError => ex
+        error << [x, ex]
       end
-    rescue StandardError => ex;
-      [x, ex]
     end
-  }.select { |x| not x.nil?}
+  }
+
+  success.select! { |x| not x.nil?}
+  error.select! { |x| not x.nil?}
+
+  {success: success, error: error}
 end
